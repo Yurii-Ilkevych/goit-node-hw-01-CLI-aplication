a0317ae8290660cb49921104ad8b1ed8fdb7026f
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-07-14 16:25:42 +0300

    optimization

--- contacts.js
+++ contacts.js
@@ -7,16 +7,11 @@ const fs = require("fs").promises;
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
-  try {
     const response = await fs.readFile(contactsPath);
-    return JSON.parse(response.toString());
-  } catch (error) {
-    return error.message;
-  }
+    return JSON.parse(response.toString()) || null;
 }
 
 async function getContactById(contactId) {
-  try {
     const response = await fs.readFile(contactsPath);
     const contacts = JSON.parse(response.toString());
     const foundedContact = contacts.filter(
@@ -27,13 +22,9 @@ async function getContactById(contactId) {
     } else {
       return null;
     }
-  } catch (error) {
-    return error.message;
-  }
 }
 
 async function removeContact(contactId) {
-  try {
     const response = await fs.readFile(contactsPath);
     const contacts = JSON.parse(response.toString());
     const remainedContacts = contacts.filter(
@@ -45,40 +36,25 @@ async function removeContact(contactId) {
     } else {
       return null;
     }
-  } catch (error) {
-    return error.message;
-  }
 }
 
 function doDelete(remainedContacts) {
-  try {
     fs.writeFile(contactsPath, JSON.stringify(remainedContacts));
-  } catch (error) {
-    console.log("404");
-  }
 }
 
 async function addContact(name, email, phone) {
   const id = uuidv1();
   const newContact = { id, name, email, phone };
 
-  try {
     const response = await fs.readFile(contactsPath);
     const allContacts = JSON.parse(response.toString());
     return doWrite(newContact, allContacts);
-  } catch (error) {
-    return error.message;
-  }
 }
 
 function doWrite(newContact, allContacts) {
   allContacts.push(newContact);
-  try {
     fs.writeFile(contactsPath, JSON.stringify(allContacts));
-    return newContact;
-  } catch (error) {
-    console.log("404");
-  }
+    return newContact || null;
 }
 
 module.exports = {
